c16dbaa490e8a49f6f65f84836b65bc2671e5276
JAIMEGFG <jaimegfg@gmail.com>
2019-12-11 23:08:15 +0100

    Práctica Equipos

--- app/src/main/java/com/jaime/equipos/utils/DataSet.java
+++ app/src/main/java/com/jaime/equipos/utils/DataSet.java
@@ -55,30 +55,30 @@ public class DataSet {
     }
     public ArrayList jugadoresMadrid(){
         ArrayList<Jugadores> lista = new ArrayList<>();
-        lista.add(new Jugadores("Alphonse  Areola",true,R.drawable.areola));
-        lista.add(new Jugadores("Thibaut Courtois",true,R.drawable.areola));
-        lista.add(new Jugadores("Daniel Carvajal",true,R.drawable.areola));
-        lista.add(new Jugadores("Eder Militao",false,R.drawable.areola));
-        lista.add(new Jugadores("Sergio Ramos",false,R.drawable.areola));
-        lista.add(new Jugadores("Rafael Varane",false,R.drawable.areola));
-        lista.add(new Jugadores("Marelo Vieira",false,R.drawable.areola));
-        lista.add(new Jugadores("Álvaro Odriozola",false,R.drawable.areola));
-        lista.add(new Jugadores("Ferd Mendy",false,R.drawable.areola));
-        lista.add(new Jugadores("Tony Kroos",false,R.drawable.areola));
-        lista.add(new Jugadores("Luka Modric",false,R.drawable.areola));
-        lista.add(new Jugadores("Carlos Casemiro",false,R.drawable.areola));
-        lista.add(new Jugadores("Fede Valverde",false,R.drawable.areola));
-        lista.add(new Jugadores("James Rodriguez",false,R.drawable.areola));
-        lista.add(new Jugadores("Francisco Alarcón",false,R.drawable.areola));
-        lista.add(new Jugadores("Eden Hazard",false,R.drawable.areola));
-        lista.add(new Jugadores("Karim Bencema",false,R.drawable.areola));
-        lista.add(new Jugadores("Gareth Bale",false,R.drawable.areola));
-        lista.add(new Jugadores("Lucas Vázquez",false,R.drawable.areola));
-        lista.add(new Jugadores("Luka Jóvic",false,R.drawable.areola));
-        lista.add(new Jugadores("Marco Asensio",false,R.drawable.areola));
-        lista.add(new Jugadores("Brahim Diaz",false,R.drawable.areola));
-        lista.add(new Jugadores("Vinicius Junior ",false,R.drawable.areola));
-        lista.add(new Jugadores("Rodrygo Goes",false,R.drawable.areola));
+        lista.add(new Jugadores("Alphonse  Areola",false,R.drawable.areola));
+        lista.add(new Jugadores("Thibaut Courtois",true,R.drawable.curtois));
+        lista.add(new Jugadores("Daniel Carvajal",true,R.drawable.carvajal));
+        lista.add(new Jugadores("Eder Militao",false,R.drawable.militao));
+        lista.add(new Jugadores("Sergio Ramos",true,R.drawable.ramos));
+        lista.add(new Jugadores("Rafael Varane",true,R.drawable.varane));
+        lista.add(new Jugadores("Marelo Vieira",true,R.drawable.marcelo));
+        lista.add(new Jugadores("Álvaro Odriozola",false,R.drawable.odriozola));
+        lista.add(new Jugadores("Ferd Mendy",false,R.drawable.mendy));
+        lista.add(new Jugadores("Tony Kroos",true,R.drawable.kros));
+        lista.add(new Jugadores("Luka Modric",true,R.drawable.modric));
+        lista.add(new Jugadores("Carlos Casemiro",true,R.drawable.casemiro));
+        lista.add(new Jugadores("Fede Valverde",false,R.drawable.valverde));
+        lista.add(new Jugadores("James Rodriguez",true,R.drawable.james));
+        lista.add(new Jugadores("Francisco Alarcón",false,R.drawable.isco));
+        lista.add(new Jugadores("Eden Hazard",true,R.drawable.hazard));
+        lista.add(new Jugadores("Karim Bencema",true,R.drawable.bencema));
+        lista.add(new Jugadores("Gareth Bale",false,R.drawable.bale));
+        lista.add(new Jugadores("Lucas Vázquez",true,R.drawable.lucasv));
+        lista.add(new Jugadores("Luka Jóvic",false,R.drawable.jovic));
+        lista.add(new Jugadores("Marco Asensio",false,R.drawable.asensio));
+        lista.add(new Jugadores("Brahim Diaz",false,R.drawable.brahim));
+        lista.add(new Jugadores("Vinicius Junior ",true,R.drawable.vinicius));
+        lista.add(new Jugadores("Rodrygo Goes",false,R.drawable.rodrygo));
 
         return lista;
     }
